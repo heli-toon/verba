dfccd4cbfdbd3df9725901e69e21217e90c40190
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2024-01-02 14:38:13 +0000

    Add a persistent night theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 // import { Link } from "react-router-dom";
-import logo from '../assets/images/dictionary.png'
+import logo from '../assets/images/logo.png'
 export default function Navbar() {
   return (
     <>
--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,15 +1,36 @@
 import { useEffect, useState } from "react";
 
 export default function Settings() {
-  // useState()
-  // useEffect()
+  // Persisting the theme
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  useEffect(() => {
+    if (localStorage.getItem("isDarkTheme") === "true") {
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkTheme) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+    localStorage.setItem("isDarkTheme", isDarkTheme);
+  }, [isDarkTheme]);
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+    localStorage.setItem("isDarkTheme", !isDarkTheme);
+  };
   return (
     <>
       <main>
         <section className="content">
           <div className="settings">
             <h1>Settings</h1>
-            <h2 className="settings-title" id="themes">Themes</h2>
+            <h2 className="settings-title" id="themes">
+              Themes
+            </h2>
             <div className="themepref">
               <label htmlFor="toggle">Night Mode</label>
               <label htmlFor="toggle" className="toggle">
@@ -18,19 +39,22 @@ export default function Settings() {
                   name="theme"
                   id="toggle"
                   type="checkbox"
-                  onClick="toggleDarkMode()"
+                  checked={isDarkTheme}
+                  onChange={toggleTheme}
                 />
                 <span className="slider"></span>
               </label>
             </div>
-            <h2 className="settings-title" id="accent">Accent Color</h2>
+            <h2 className="settings-title" id="accent">
+              Accent Color
+            </h2>
             <div className="accent-colors">
-                <a href="#" className="accent-color default"></a>
-                <a href="#" className="accent-color orange"></a>
-                <a href="#" className="accent-color pink"></a>
-                <a href="#" className="accent-color purple"></a>
-                <a href="#" className="accent-color gray"></a>
-                <a href="#" className="accent-color green"></a>
+              <a href="#" className="accent-color default"></a>
+              <a href="#" className="accent-color orange"></a>
+              <a href="#" className="accent-color pink"></a>
+              <a href="#" className="accent-color purple"></a>
+              <a href="#" className="accent-color gray"></a>
+              <a href="#" className="accent-color green"></a>
             </div>
           </div>
         </section>
