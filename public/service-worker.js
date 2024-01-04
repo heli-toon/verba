5939ff41d3849cf9388db3afa025e490a3d88ddd
Salay Abdul Muhaimin Kanton <helitoon@outlook.com>
2024-01-04 13:24:35 +0000

    Add vite-pwa-plugin dep

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,14 @@
-import workbox from "workbox";
+import workbox from "./workbox";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js');
+fetch('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js')
+  .then((response) => response.text())
+  .then((text) => {
+    eval(text);
+    const { registerRoute } = workbox.routing;
+    const { CacheFirst } = workbox.strategies;
 
-const { registerRoute } = workbox.routing;
-const { CacheFirst } = workbox.strategies;
-
-registerRoute(
-  ({ url }) => true,
-  new CacheFirst()
-);
+    registerRoute(
+      ({ url }) => true,
+      new CacheFirst()
+    );
+  });
