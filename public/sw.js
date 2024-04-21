32690dc808812055f59e2dc96655e533412590b3
Salay Abdul Muhaimin Kanton <abdulkanton2005@gmail.com>
2024-04-21 20:30:38 +0000

    Fixed sw.js error

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,11 @@
 import workbox from "./workbox";
 
-fetch('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js')
+fetch("https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js")
   .then((response) => response.text())
   .then((text) => {
     eval(text);
     const { registerRoute } = workbox.routing;
     const { CacheFirst } = workbox.strategies;
 
-    registerRoute(
-      ({ url }) => true,
-      new CacheFirst()
-    );
+    registerRoute(({ url }) => true, new CacheFirst());
   });
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { fetchWordDefinition } from './Api.jsx';
 
 export default function SearchHeader() {
